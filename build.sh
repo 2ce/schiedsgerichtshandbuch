624747b8821dd4c5ed357186ee4504a78bd432d0
Florian Zumkeller-Quast <branleb@googlemail.com>
2015-10-29 01:57:21 +0100

    build.sh stops after first failed build step

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Build clean auxilary files
-xelatex sghandbuch
-xelatex sghandbuch
-
+xelatex sghandbuch && \ 
+xelatex sghandbuch && \
+\
 # Build bib
-biber sghandbuch
-
+biber sghandbuch && \
+\
 # Build clean aux files
-xelatex sghandbuch
-xelatex sghandbuch
-
+xelatex sghandbuch &&  \
+xelatex sghandbuch && \
+\
 # Build index
-splitindex sghandbuch -- -s sghandbuch.ist
-
-# Build nomenclature / abbreviation index
-makeindex sghandbuch.nlo -s nomencl.ist -o sghandbuch.nls
-
+splitindex sghandbuch -- -s sghandbuch.ist && \
+\
+# Build nomenclature / abbreviation index \
+makeindex sghandbuch.nlo -s nomencl.ist -o sghandbuch.nls && \
+\
 # Build clean pdf
-xelatex sghandbuch
-xelatex sghandbuch
-xelatex sghandbuch
+xelatex sghandbuch && \
+xelatex sghandbuch && \
+xelatex sghandbuch 
