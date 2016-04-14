c3ca12d248f4f294f1ece4119adfbf201516fa10
Florian Zumkeller-Quast <branleb@googlemail.com>
2016-04-14 12:05:18 +0200

    added draft build mode. just use './build.sh draft' command

--- build.sh
+++ build.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-# Build clean auxilary files
-xelatex sghandbuch && \ 
-xelatex sghandbuch && \
-\
-# Build bib
-biber sghandbuch && \
-\
-# Build clean aux files
-xelatex sghandbuch &&  \
-xelatex sghandbuch && \
-\
-# Build index
-splitindex sghandbuch -- -s sghandbuch.ist && \
-\
-# Build nomenclature / abbreviation index \
-makeindex sghandbuch.nlo -s nomencl.ist -o sghandbuch.nls && \
-\
-# Build clean pdf
-xelatex sghandbuch && \
-xelatex sghandbuch && \
-xelatex sghandbuch 
+FILE="sghandbuch.template.tex";
+PROJECTNAME=${FILE/%.template.tex/};
+MODE="$1";
+if [ "${MODE}" != "draft" ]; then
+    MODE="final"
+fi;
+
+if [ ! -e "${FILE}" ]; then
+    echo "Texfile ${FILE} missing!"
+else
+    TEXPATH="${FILE/%.template.tex/.tex}"
+    echo "Building buildtemp file for ${MODE}: ${TEXPATH}";
+    if [ "${MODE}" == "draft" ]; then
+        echo "\\documentclass[${MODE}]{sghandbuch}" > "${TEXPATH}";
+    else
+        echo "\\documentclass{sghandbuch}" > "${TEXPATH}";
+    fi;
+    echo "\\input{\"${FILE}\"}" >> "${TEXPATH}"
+    
+    # Build clean auxilary files
+    xelatex "${PROJECTNAME}" && \ 
+    xelatex "${PROJECTNAME}" && \
+    \
+    # Build bib
+    biber "${PROJECTNAME}" && \
+    \
+    # Build clean aux files
+    xelatex "${PROJECTNAME}" &&  \
+    xelatex "${PROJECTNAME}" && \
+    \
+    # Build index
+    splitindex "${PROJECTNAME}" -- -s "${PROJECTNAME}".ist && \
+    \
+    # Build nomenclature / abbreviation index \
+    makeindex "${PROJECTNAME}".nlo -s nomencl.ist -o "${PROJECTNAME}".nls && \
+    \
+    # Build clean pdf
+    xelatex "${PROJECTNAME}" && \
+    xelatex "${PROJECTNAME}" && \
+    xelatex "${PROJECTNAME}" && \
+    \
+    rm "$TEXPATH"
+
+fi;
